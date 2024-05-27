1ca557007b474a5044a9cad577cf8be8d2919d30
ilya11tsaryuk <ilya11tsaryuk@gmail.com>
2024-05-27 15:36:36 +0300

    deleted mock values

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -1,5 +1,4 @@
 import { getProductsByCategory, getSearchProducts } from "@/app/api/products";
-import { clothesCategory, products } from "@/app/constants";
 import { ProductsList } from "@/app/ui/components/ProductsList";
 import { notFound } from "next/navigation";
 
